31d1e3b7d07ee060d76a72a78730921434bd5a1e
Maksim Mihailovskii <maximmmd13@gmail.com>
2025-03-05 09:51:56 +0200

    Add files via upload

---
+++ ConsoleApp34/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    /// <summary>
+    /// Задача 1. Ввести массив с клавиатуры, состоящий из 14 элементов целого типа. Найти количество элементов четных по значению.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = new int[14];
+            VvodMass(omas);
+            Elements(omas);
+            Console.ReadKey();
+        }
+        public static void VvodMass(int[] omas)
+        {
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.Write($"omas[{i}]=");
+                omas[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public static void Elements(int[] omas)
+        {
+            int f = 0;
+            foreach (int element in omas)
+            {
+                if ((element & 1) == 0)
+                {
+                   f++;
+                }
+            }
+            Console.WriteLine($"Количество чётных елементов={f}");
+        }
+    }
+}
---
+++ ConsoleApp36/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleApp36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { -2, 5, -6, 7, -6, 12, 10, -9 };
+        }
+        public static void Elements (int[] omas)
+        {
+            int maxelement = omas[0];
+            int maxindex = 0;
+            int a = 0;
+            for (int i = 0; i < omas.Length; i++)
+            {
+                if (maxelement < omas[i]) 
+                { 
+                    maxindex = i;
+                }
+            }
+            a = omas[0];
+            omas[0] = omas[maxindex];
+            omas[maxindex] = a;
+        }
+    }
+}
